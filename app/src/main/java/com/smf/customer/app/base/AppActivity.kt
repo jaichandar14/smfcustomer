2a59939290260224d20b07c3407a9265c691b9e8
P Vigneshwaran <vigneshwaran.palanichamy@bpmlinks.com>
2023-02-02 14:18:04 +0530

    FEATURE - 3360(Customer app - Transparent status bar)
    - Implemented transparent status bar for all the activity.

--- app/src/main/java/com/smf/customer/app/base/AppActivity.kt
+++ app/src/main/java/com/smf/customer/app/base/AppActivity.kt
@@ -1,6 +1,13 @@
 package com.smf.customer.app.base
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
+import com.smf.customer.R
 
 abstract class AppActivity : AppCompatActivity() {
+
+    open fun setStatusBarColor() {
+        window.statusBarColor = getColor(R.color.theme_color)
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
+    }
 }
